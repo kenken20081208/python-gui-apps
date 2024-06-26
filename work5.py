9b25292aaad733bebac5cd0899376025bfdddef9
kenken20081208 <pingyej363@gmail.com>
2024-06-26 10:40:57 +0900

    work5終わり

--- work5.py
+++ work5.py
@@ -9,42 +9,30 @@ bg_color = "#333333"  # ダークグレー
 fg_color = "#FFFFFF"  # 白
 window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
-
-# リスト
-meibo = []
+moji = ["python", "HTML", "CSS"]
 
 
 def button_action():  # 関数の定義 ※ボタンが押されたときの動き
-    meibo.append(entry1.get())
-    name_list = ""
-    for a in meibo:
-        name_list += f"{a}\n"  # 入力値を取得
-    label1.config(text=f"{name_list}\n ")  # 画面に出力
+    user_input = entry1.get()  # 入力値を取得
+
+    if label1.cget("text") == user_input:
+        label1.config(text=random.choice(moji))
 
 
-# リストの中からランダムに一つ要素を取り出す
-def button_action2():
-    a = random.choice(meibo)
-    label2.config(text=a)
-
+label1 = tk.Label(window, text=random.choice(moji), bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
 
 # 入力フィールドの作成
 entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
 entry1.pack(pady=10)
 
 # ボタンの作成
-button1 = tk.Button(window, text="追加", command=button_action)
+button1 = tk.Button(window, text="ok", command=button_action)
 button1.pack(pady=10)
-# ボタン2
-button2 = tk.Button(window, text="ランダム選択", command=button_action2)
-button2.pack(pady=10)
 
 # 出力ラベルの作成
-label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
-label1.pack(pady=10)
 label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
 label2.pack(pady=10)
-
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
